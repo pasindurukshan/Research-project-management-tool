e9304209858baa1931472e4f34f6ee10334b3e3a
pathumHewage <it20185220@my.sliit.lk>
2022-06-01 22:36:26 +0530

    add workshop routes,controller,schemas

--- routes/editor-routes.js
+++ routes/editor-routes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 
 const PresentationController = require('../controllers/presentation-controller');
-
+const WorkshopController = require('../controllers/workshop-controller');
 
 router.post('/presentation', PresentationController.createPresentation);
 router.get('/presentation/single/:id', PresentationController.getSinglePresentation);
-
+router.get('/workshop/single/:id', WorkshopController.getSingleWorkshop);
+router.post('/workshop', WorkshopController.createWorkshop);
 
 
 
