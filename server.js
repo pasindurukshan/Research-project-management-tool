efe6cbf88e1f6c87340b59f7e56b12693bc0c883
pasindurukshan <it20198640@my.sliit.lk>
2022-06-05 01:12:48 +0530

    deploy

--- server.js
+++ server.js
@@ -46,9 +46,14 @@ app.use(errorHandler);
 app.use('/api/v1/users/', UserRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')))
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+  });
+} else { 
+  app.get('*', (req, res) => {
+    res.send("API is running....")
   });
 }
 
